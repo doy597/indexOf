1d8887fcf37f3652339e6d65f531eeb402530889
doy597 <64715985+doy597@users.noreply.github.com>
2020-06-16 15:57:22 +0900

    Add files via upload

---
+++ mondai2.java
@@ -0,0 +1,32 @@
+class Moji{
+	public static void main(String args[]){
+	String a,b,c;
+	
+	a = args.length[];
+	
+
+	if(a==0){
+		System.out.println("文字列を入力してください");	
+		return;
+	}
+	else{
+
+
+	int x1,x2;
+
+	x1 = a.indexOf("。",) + 1;
+	x2 = a.indexOf("。",19) + 1;
+
+	
+	System.out.println("aに格納されている文字数は28文字です");
+
+	System.out.println("1つめの「。」は" + x1 + "文字目にあります");
+	System.out.println("２つめの「。」は"+ x2 + "文字目にあります");
+
+	System.out.println(b);
+	System.out.println(c);
+		}
+
+	}
+
+}
